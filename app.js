04c890b64bedce4792a4dd4ebc436b24e9a23002
Phil Skaggs <prskaggsjr@gmail.com>
2016-05-23 13:35:23 -0600

    Basic site view

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
