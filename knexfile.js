6c5fd1a894d13202b8ff2018a270082b6b9297e9
Conrad G Bailey <grhmbailey@gmail.com>
2016-05-24 11:08:10 -0600

    login and sign up ready to be hooked up for testing

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL
+  },
+
+};
---
+++ migrations/20160524101814_create_users.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', table =>{
+    table.increments();
+    table.string('email').notNullable().unique();
+    table.string('password_hash').notNullable();
+  })
+
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
