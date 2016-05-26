5253e097ad09a2a8cdc450b16920b30d12703881
Conrad G Bailey <grhmbailey@gmail.com>
2016-05-26 10:42:19 -0600

    merge complete

--- public/auth/authcontroller.js
+++ public/auth/authcontroller.js
@@ -7,8 +7,13 @@
     vm.handleAuth = function(){
       console.log(authType);
     return  UserService.attemptAuth(authType, vm.user)
-    .then(function() {
-      $state.go('loggedin', null, { reload: true })
+    .then(function(response) {
+      if(response.error){
+        vm.error = response.error;
+        $state.go('login', null, {reload: true})
+      }else {
+        $state.go('loggedin', null, { reload: true })
+      }
     })
 
     }
