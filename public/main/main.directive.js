39e7e1f07fc9a8590d6e9be7eae420c9cd62ba34
Blake Netzeband <blake.netzeband@gmail.com>
2016-05-26 11:23:06 -0600

    changed log / socket comments. added chord feature

--- public/main/main.directive.js
+++ public/main/main.directive.js
@@ -21,44 +21,86 @@
 
         var mySocket = BMFactory.mySocket;
 
+        // CHORDS
+        $scope.playChordC = function() {
+          mySocket.emit('chordC');
+          console.log('PLAYING CHORD C IN CLIENT');
+        };
+
+        $scope.playChordD = function() {
+          mySocket.emit('chordD');
+          console.log('PLAYING CHORD D IN CLIENT');
+        };
+
+        $scope.playChordE = function() {
+          mySocket.emit('chordE');
+          console.log('PLAYING CHORD E IN CLIENT');
+        };
+
+        $scope.playChordF = function() {
+          mySocket.emit('chordF');
+          console.log('PLAYING CHORD F IN CLIENT');
+        };
+
+        $scope.playChordG = function() {
+          mySocket.emit('chordG');
+          console.log('PLAYING CHORD G IN CLIENT');
+        };
+
+        $scope.playChordA = function() {
+          mySocket.emit('chordA');
+          console.log('PLAYING CHORD A IN CLIENT');
+        };
+
+        $scope.playChordB = function() {
+          mySocket.emit('chordB');
+          console.log('PLAYING CHORD B IN CLIENT');
+        };
+
+        $scope.playChordC2 = function() {
+          mySocket.emit('chordC2');
+          console.log('PLAYING CHORD C2 IN CLIENT');
+        };
+
+        // NOTES
         $scope.playNoteC = function() {
-          mySocket.emit('noteC:on');
-          console.log('NOTE: C ON');
+          mySocket.emit('noteC');
+          console.log('PLAYING NOTE C IN CLIENT');
         };
 
         $scope.playNoteD = function() {
-          mySocket.emit('noteD:on');
-          console.log('NOTE: D ON');
+          mySocket.emit('noteD');
+          console.log('PLAYING NOTE D IN CLIENT');
         };
 
         $scope.playNoteE = function() {
-          mySocket.emit('noteE:on');
-          console.log('NOTE: E ON');
+          mySocket.emit('noteE');
+          console.log('PLAYING NOTE E IN CLIENT');
         };
 
         $scope.playNoteF = function() {
-          mySocket.emit('noteF:on');
-          console.log('NOTE: F ON');
+          mySocket.emit('noteF');
+          console.log('PLAYING NOTE F IN CLIENT');
         };
 
         $scope.playNoteG = function() {
-          mySocket.emit('noteG:on');
-          console.log('NOTE: G ON');
+          mySocket.emit('noteG');
+          console.log('PLAYING NOTE G IN CLIENT');
         };
 
         $scope.playNoteA = function() {
-          mySocket.emit('noteA:on');
-          console.log('NOTE: A ON');
+          mySocket.emit('noteA');
+          console.log('PLAYING NOTE A IN CLIENT');
         };
 
         $scope.playNoteB = function() {
-          mySocket.emit('noteB:on');
-          console.log('NOTE: B ON');
+          mySocket.emit('noteB');
+          console.log('PLAYING NOTE B IN CLIENT');
         };
 
         $scope.playNoteC2 = function() {
-          mySocket.emit('noteC2:on');
-          console.log('NOTE: C2 ON');
+          mySocket.emit('noteC2');
+          console.log('PLAYING NOTE C2 IN CLIENT');
         };
 
     }
