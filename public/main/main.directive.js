a55fd2be00c491af894320f27d532cfd10df73c3
Blake Netzeband <blake.netzeband@gmail.com>
2016-05-26 22:54:49 -0600

    added slider function. bpm successfully transfers to server. unable to test arduino socket.

--- public/main/main.directive.js
+++ public/main/main.directive.js
@@ -1,128 +1,147 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    angular.module('app')
-        .directive('bmMain', mainDirective);
+  angular.module('app')
+  .directive('bmMain', mainDirective);
 
-    function mainDirective() {
-        return {
-            restrict: 'E',
-            templateUrl: '/main/main.directive.html',
-            controller: controller,
-            controllerAs: 'main',
-            bindToController: true
-        };
+  function mainDirective() {
+    return {
+      restrict: 'E',
+      templateUrl: '/main/main.directive.html',
+      controller: controller,
+      controllerAs: 'main',
+      bindToController: true
+    };
+  }
+
+  controller.$inject = ['$scope', 'BMFactory', '$timeout'];
+
+  function controller($scope, BMFactory, $timeout) {
+    var main = this;
+    var mySocket = BMFactory.mySocket;
+
+    main.bpm = {
+      value: 200,
+      options: {
+        floor: 100,
+        ceil: 2000,
+        step: 50,
+        showSelectionBar: true,
+        getPointerColor: function(value) {
+            if (value <= 500) return 'yellow';
+            if (value <= 1000) return 'orange';
+            if (value <= 1500) return 'green';
+            return '#2AE02A';
+        }
+      },
+    };
+
+    main.isLoggedIn = false;
+
+    if ( localStorage.getItem('jwtToken') ) {
+      main.isLoggedIn = !main.isLoggedIn;
     }
 
-    controller.$inject = ['$scope', 'BMFactory', '$timeout'];
-
-    function controller($scope, BMFactory, $timeout) {
-        var main = this;
-        var mySocket = BMFactory.mySocket;
-
-        main.isLoggedIn = false;
-
-        if ( localStorage.getItem('jwtToken') ) {
-            main.isLoggedIn = !main.isLoggedIn;
-        }
-
-        main.logout = function() {
-            BMFactory.logOut();
-            main.isLoggedIn = !main.isLoggedIn;
-        }
-
-        // main.isAuth = function () {
-        //
-        // };
-
-        // SEQUENCE
-        main.playSequence = function (sequence) {
-          mySocket.emit('sequence', sequence.toUpperCase());
-          console.log('in directive', sequence.toUpperCase());
-        }
-
-        // CHORDS
-        $scope.playChordC = function() {
-          mySocket.emit('chordC');
-          console.log('PLAYING CHORD C IN CLIENT');
-        };
-
-        $scope.playChordD = function() {
-          mySocket.emit('chordD');
-          console.log('PLAYING CHORD D IN CLIENT');
-        };
-
-        $scope.playChordE = function() {
-          mySocket.emit('chordE');
-          console.log('PLAYING CHORD E IN CLIENT');
-        };
-
-        $scope.playChordF = function() {
-          mySocket.emit('chordF');
-          console.log('PLAYING CHORD F IN CLIENT');
-        };
-
-        $scope.playChordG = function() {
-          mySocket.emit('chordG');
-          console.log('PLAYING CHORD G IN CLIENT');
-        };
-
-        $scope.playChordA = function() {
-          mySocket.emit('chordA');
-          console.log('PLAYING CHORD A IN CLIENT');
-        };
-
-        $scope.playChordB = function() {
-          mySocket.emit('chordB');
-          console.log('PLAYING CHORD B IN CLIENT');
-        };
-
-        $scope.playChordC2 = function() {
-          mySocket.emit('chordC2');
-          console.log('PLAYING CHORD C2 IN CLIENT');
-        };
-
-        // NOTES
-        $scope.playNoteC = function() {
-          mySocket.emit('noteC');
-          console.log('PLAYING NOTE C IN CLIENT');
-        };
-
-        $scope.playNoteD = function() {
-          mySocket.emit('noteD');
-          console.log('PLAYING NOTE D IN CLIENT');
-        };
-
-        $scope.playNoteE = function() {
-          mySocket.emit('noteE');
-          console.log('PLAYING NOTE E IN CLIENT');
-        };
-
-        $scope.playNoteF = function() {
-          mySocket.emit('noteF');
-          console.log('PLAYING NOTE F IN CLIENT');
-        };
-
-        $scope.playNoteG = function() {
-          mySocket.emit('noteG');
-          console.log('PLAYING NOTE G IN CLIENT');
-        };
-
-        $scope.playNoteA = function() {
-          mySocket.emit('noteA');
-          console.log('PLAYING NOTE A IN CLIENT');
-        };
-
-        $scope.playNoteB = function() {
-          mySocket.emit('noteB');
-          console.log('PLAYING NOTE B IN CLIENT');
-        };
-
-        $scope.playNoteC2 = function() {
-          mySocket.emit('noteC2');
-          console.log('PLAYING NOTE C2 IN CLIENT');
-        };
-
+    main.logout = function() {
+      BMFactory.logOut();
+      main.isLoggedIn = !main.isLoggedIn;
     }
+
+    // main.isAuth = function () {
+    //
+    // };
+
+    // SEQUENCE
+    main.playSequence = function (sequence) {
+      let bpm = main.bpm.value;
+      let seq = sequence.toUpperCase();
+      mySocket.emit('sequence', {seq, bpm});
+      console.log('in directive', sequence.toUpperCase());
+      console.log('in directive bpm', main.bpm.value);
+    }
+
+    // CHORDS
+    $scope.playChordC = function() {
+      mySocket.emit('chordC');
+      console.log('PLAYING CHORD C IN CLIENT');
+    };
+
+    $scope.playChordD = function() {
+      mySocket.emit('chordD');
+      console.log('PLAYING CHORD D IN CLIENT');
+    };
+
+    $scope.playChordE = function() {
+      mySocket.emit('chordE');
+      console.log('PLAYING CHORD E IN CLIENT');
+    };
+
+    $scope.playChordF = function() {
+      mySocket.emit('chordF');
+      console.log('PLAYING CHORD F IN CLIENT');
+    };
+
+    $scope.playChordG = function() {
+      mySocket.emit('chordG');
+      console.log('PLAYING CHORD G IN CLIENT');
+    };
+
+    $scope.playChordA = function() {
+      mySocket.emit('chordA');
+      console.log('PLAYING CHORD A IN CLIENT');
+    };
+
+    $scope.playChordB = function() {
+      mySocket.emit('chordB');
+      console.log('PLAYING CHORD B IN CLIENT');
+    };
+
+    $scope.playChordC2 = function() {
+      mySocket.emit('chordC2');
+      console.log('PLAYING CHORD C2 IN CLIENT');
+    };
+
+    // NOTES
+    $scope.playNoteC = function() {
+      mySocket.emit('noteC');
+      console.log('PLAYING NOTE C IN CLIENT');
+    };
+
+    $scope.playNoteD = function() {
+      mySocket.emit('noteD');
+      console.log('PLAYING NOTE D IN CLIENT');
+    };
+
+    $scope.playNoteE = function() {
+      mySocket.emit('noteE');
+      console.log('PLAYING NOTE E IN CLIENT');
+    };
+
+    $scope.playNoteF = function() {
+      mySocket.emit('noteF');
+      console.log('PLAYING NOTE F IN CLIENT');
+    };
+
+    $scope.playNoteG = function() {
+      mySocket.emit('noteG');
+      console.log('PLAYING NOTE G IN CLIENT');
+    };
+
+    $scope.playNoteA = function() {
+      mySocket.emit('noteA');
+      console.log('PLAYING NOTE A IN CLIENT');
+    };
+
+    $scope.playNoteB = function() {
+      mySocket.emit('noteB');
+      console.log('PLAYING NOTE B IN CLIENT');
+    };
+
+    $scope.playNoteC2 = function() {
+      mySocket.emit('noteC2');
+      console.log('PLAYING NOTE C2 IN CLIENT');
+    };
+
+  }
 
 })();
